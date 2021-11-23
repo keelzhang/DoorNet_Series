30a744089822c9afd0917b83944eccfe2756618e
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-11-23 22:25:12 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -48,6 +48,12 @@ git clone --depth=1 https://github.com/NateLol/luci-app-oled
 # Add ServerChan
 git clone --depth=1 https://github.com/tty228/luci-app-serverchan
 
+# Add ddnsto & linkease
+svn co https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto
+svn co https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-linkease
+svn co https://github.com/linkease/nas-packages/trunk/network/services/ddnsto
+svn co https://github.com/linkease/nas-packages/trunk/network/services/linkease
+
 # Add luci-app-diskman
 git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman
 mkdir parted
