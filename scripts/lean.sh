1d02a13c2b749f40a6138d5011b83f97fbcee240
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-12-12 13:57:12 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -180,8 +180,8 @@ sed -i "s/OpenWrt /DHDAXCW $(TZ=UTC-8 date "+%Y%m%d") @ FusionWrt /g" package/le
 # popd
 
 # 修复无线mac问题
-# rm -rf package/kernel/rtl8821cu
-# svn co https://github.com/LubanCat/DoorNet-OpenWrt/trunk/package/kernel/rtl8821cu package/kernel/rtl8821cu
+rm -rf package/kernel/rtl8821cu
+svn co https://github.com/LubanCat/DoorNet-OpenWrt/trunk/package/kernel/rtl8821cu package/kernel/rtl8821cu
 svn co https://github.com/LubanCat/DoorNet-OpenWrt/trunk/target/linux/rockchip/armv8/base-files/usr/bin target/linux/rockchip/armv8/base-files/usr/bin   
 svn co https://github.com/LubanCat/DoorNet-OpenWrt/trunk/target/linux/rockchip/armv8/base-files/etc/rc.d  target/linux/rockchip/armv8/base-files/etc/rc.d
 rm -rf package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
