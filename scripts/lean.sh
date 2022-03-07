f4c3993a74925bc14fd35fb7618bc8de92fd7b92
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-03-07 15:28:37 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -182,7 +182,7 @@ make && sudo make install
 popd
 
 rm -rf ./package/kernel/linux/modules/video.mk
-wget -P package/kernel/linux/modules/ https://github.com/immortalwrt/immortalwrt/raw/master/package/kernel/linux/modules/video.mk
+# wget -P package/kernel/linux/modules/ https://github.com/immortalwrt/immortalwrt/raw/master/package/kernel/linux/modules/video.mk
 
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
