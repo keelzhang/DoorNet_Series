ebe6e8df413052b70d4efe959bf567879b7aded7
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-05-22 22:41:38 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -21,10 +21,6 @@ rm -rf openwrt-package/luci-app-verysync
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
 
-# Add luci-app-netdata
-rm -rf ../../feeds/luci/applications/luci-app-netdata
-git clone --depth=1 https://github.com/sirpdboy/luci-app-netdata
-
 # Add luci-app-ssr-plus
 git clone --depth=1 https://github.com/fw876/helloworld
 
